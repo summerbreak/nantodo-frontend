93b309e6dd7e204373b7b0835470c5fdb8229d8f
Hugo <1621740564@qq.com>
2023-12-01 10:38:02 +0800

    creat new branch course

---
+++ src/components/course-search.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="search">
+    <el-input
+        v-model="course"
+        size="large"
+        placeholder="课程搜索"
+        :suffix-icon="Search"
+        style="width: 50%"
+        @change="querySearch"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {onMounted, ref} from 'vue'
+import {Search} from '@element-plus/icons-vue'
+
+let course: string="";
+const querySearch = () => {
+
+}
+
+
+onMounted(() => {
+})
+</script>
+
+<style>
+.search {
+  padding: 20px;
+  text-align: center;
+  width: 100%;
+}
+
+.my-autocomplete li {
+  line-height: normal;
+  padding: 7px;
+}
+
+.my-autocomplete li .name {
+  text-overflow: ellipsis;
+  overflow: hidden;
+}
+
+.my-autocomplete li .addr {
+  font-size: 12px;
+  color: #b4b4b4;
+}
+
+.my-autocomplete li .highlighted .addr {
+  color: #ddd;
+}
+</style>
--- src/pages/course.vue
+++ src/pages/course.vue
@@ -1,3 +1,24 @@
 <template>
-    <h2>这里是课程</h2>
+  <course-search/>
+  <el-row class="outside">
+    <el-col
+        v-for="(o, index) in 12"
+        :key="o"
+        :span="6"
+        :offset="0"
+    >
+      <course-grid teacher="67"/>
+    </el-col>
+  </el-row>
 </template>
+<script setup>
+import CourseSearch from "../components/course-search.vue";
+import CourseGrid from "../components/course-grid.vue";
+
+</script>
+<style scoped>
+.outside {
+  margin-left: 100px;
+  margin-right: 100px
+}
+</style>
