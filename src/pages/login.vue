bb0ce4b068136e64e75797cef17dd4768a871c72
Young <729883294@qq.com>
2023-12-05 21:41:32 +0800

    登录页

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,29 +1,28 @@
 <template>
     <div class="login-container">
         <div class="background-image"></div>
+
         <div class="login-box">
-            <div class="t-tab-top">登录 </div>
+            <div class="t-tab-top">登 录</div>
             <div class="container-fluid" style="padding-bottom: 24px;">
-                <div class="form-horizontal">
-                    <div style="padding:32px 48px;">
-                        <div class="form-group" style="margin-bottom: 4px;">
-                            <input type="text" class="form-control form-bg" name="email"
-                                style="background-color: #f8fafc;height: 48px;" placeholder="请输入账号">
-                        </div>
+                <div class="form">
+                    <div style="padding:15px 5px;">
                         <div class="form-group" style="margin-bottom: 10px;">
-                            <input type="password" id="id_login_password" class="form-control form-bg margin-top-20"
-                                style="height: 48px;" name="password" placeholder="请输入密码">
+                            <el-input v-model="account" placeholder="请输入账号" height="20px" size="large" />
                         </div>
-                        <div class="form-group has-error" style="margin-bottom: 0;">
-                            <p class="help-block" style="font-size: 12px; margin-top: 0">
-                            </p>
+                        <div class="form-group" style="margin-bottom: 20px;">
+                            <el-input v-model="password" type="password" placeholder="请输入密码" size="large" show-password />
                         </div>
-                        <div class="form-group" style="margin-bottom: 22px;">
-                            <a id="id_login_button" class="btn btn-success btn-block btn-login" data-id="login">登录</a>
+                        <div class="form-group" style=" margin-bottom: 22px; display: flex; justify-content: center;">
+                            <el-button type="warning" style="width: 100%; height: 42px;font-size: 18px;" @click="login">登
+                                录</el-button>
                         </div>
                         <div class="form-group margin-bottom-15 margin-top-35">
-                            <a target="_blank" style="color:#3fb5df;float:right">
-                                <span style="color: black;">忘记密码？</span>
+                            <a target="_blank" style="color:#3fb5df;float:left;margin-left: 20px;">
+                                <span style="color: black;" @click="register">立即注册</span>
+                            </a>
+                            <a target="_blank" style="color:#3fb5df;float:right;margin-right:29px;">
+                                <span style="color: black;">忘记密码</span>
                             </a>
                         </div>
                     </div>
@@ -34,8 +33,18 @@
     </div>
 </template>
   
+
 <script>
+import { ref } from 'vue'
 export default {
+    setup() {
+        const account = ref('')
+        const password = ref('')
+        return {
+            account,
+            password,
+        }
+    },
     data() {
         return {
             username: '',
@@ -49,9 +58,23 @@ export default {
         },
     },
 };
-</script>
-  
+</script> 
+
 <style scoped>
+:deep(.el-input__inner) {
+    height: 48px;
+    border-radius: 8px;
+    /* border: 1px solid #db8916; */
+    background-color: #ffffff;
+    color: #db8916;
+    font-size: 16px;
+    font-weight: 400;
+    font-style: normal;
+    font-stretch: normal;
+    letter-spacing: normal;
+    padding: 0 10px;
+}
+
 .login-container {
     position: relative;
     height: 100vh;
@@ -65,52 +88,35 @@ export default {
     width: 100%;
     height: calc(100% - 80px);
     background: url('../assets/pic/autumn.jpg') center/cover no-repeat;
-    z-index: -1;
+    z-index: 0;
     /* 将背景图片放到最底层 */
 }
 
 .login-box {
-    border-radius: 5px;
+    border-radius: 8px;
+    /* 圆角 */
     position: absolute;
-    top: 50%;
+    top: 43%;
     left: 75%;
     transform: translate(-50%, -50%);
-    width: 300px;
-    padding: 20px;
+    width: 325px;
+    padding: 30px;
     background: rgba(255, 255, 255, 0.8);
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.6);
+}
+
+.t-tab-top {
+    font-size: 30px;
+    font-weight: 700;
+    color: #000000;
+    text-align: center;
+    padding: 5px 0 20px 0;
+    border-bottom: 1px solid #db8916;
+    margin-bottom: 20px;
 }
 
 .form {
     display: flex;
     flex-direction: column;
-}
-
-.label {
-    margin-bottom: 5px;
-}
-
-.input {
-    padding: 8px;
-    margin-bottom: 10px;
-}
-
-.button {
-    padding: 10px;
-    background-color: #007bff;
-    color: #fff;
-    border: none;
-    cursor: pointer;
-}
-
-
-.t-tab-top {
-    font-size: 24px;
-    font-weight: 700;
-    color: #333;
-    text-align: center;
-    padding: 20px 0;
-    border-bottom: 1px solid #e5e5e5;
-    margin-bottom: 20px;
 }
 </style>
