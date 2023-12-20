683e4ce2b803e6dcd150354218576e7f1098f427
zh <907415901@qq.com>
2023-12-20 16:55:56 +0800

    stamp

--- src/components/task-card.vue
+++ src/components/task-card.vue
@@ -1,80 +1,51 @@
 <template>
   <div class="task-card" :class="{ completed: isCompleted }">
-    <div class="header">
+    <div class="header" @click="moreDetails">
       <div class="icon-and-title">
         <div class="star">
-          <i
-            @click="toggleStar"
-            :class="isStarred ? 'bi bi-star-fill' : 'bi bi-star'"
-            :style="`font-size: 25px; color: ${
-              isStarred ? '#ffe4b5' : '#888'
-            }; font-style: normal;`"
-          ></i>
+          <i @click="toggleStar" :class="isStarred ? 'bi bi-star-fill' : 'bi bi-star'" :style="`font-size: 25px; color: ${isStarred ? '#ffe4b5' : '#888'
+            }; font-style: normal;`"></i>
         </div>
-
-        <h2 class="title">{{ myTitle }}</h2>
+        <h2 class="title" >{{ myTitle }}</h2>
       </div>
 
-      <p class="date">发布时间: {{ myReleaseTime.toLocaleString('af') }}</p>
+      <div class="date" @click="moreDetails">发布时间: {{ myReleaseTime.toLocaleString('af') }}</div>
     </div>
-    <div class="group-name">
+    <span class="group-name">
       来自
-      <router-link tag="button" :to="{path: '/group', query: {id: groupId}}">
-        {{groupName}}
+      <router-link tag="button" :to="{ path: '/group', query: { id: groupId } }">
+        {{ groupName }}
       </router-link>
-    </div>
-    <div class="content">
-      <p class="content-text">{{ myContent }}</p>
-    </div>
+    </span>
+    <p class="content-text" @click="moreDetails">{{ myContent }}</p>
     <div class="completion-overlay" v-if="isCompleted">
-      <i
-        class="bi bi-check-lg"
-        style="font-size: 150px; color: #a9a9a9"
-        @click="undoCompletion"
-      ></i>
+      <i class="bi bi-check-lg" style="font-size: 150px; color: #a9a9a9" @click="undoCompletion"></i>
+    </div>
+    <div class="urgen-alarm" v-if="urgent&&!isCompleted">
+      <img src="../assets/pic/urgent.png" class="urgent-img">
     </div>
     <div class="footer">
-      <el-tooltip
-        class="box-item"
-        content="Chatgpt描述"
-        placement="top-start"
-        effect="light"
-      >
+      <el-tooltip class="box-item" content="Chatgpt描述" placement="top-start" effect="light">
         <button @click="handleButtonClick('Button 1')">
           <i class="bi bi-robot"></i>
         </button>
       </el-tooltip>
 
-      <el-tooltip
-        class="box-item"
-        content="更多详情"
-        placement="top-start"
-        effect="light"
-      >
-        <button @click="moreDetails">
-          <i class="bi bi-three-dots"></i></button
-      ></el-tooltip>
-      <el-tooltip
-        class="box-item"
-        content="完成"
-        placement="top-start"
-        effect="light"
-      >
+      <el-tooltip class="box-item" content="写作业" placement="top-start" effect="light">
+        <button @click="doHomework">
+          <i class="bi bi-pencil-square"></i></button></el-tooltip>
+      <el-tooltip class="box-item" content="完成" placement="top-start" effect="light">
         <button @click="completeTask">
-          <i class="bi bi-check-lg"></i></button
-      ></el-tooltip>
+          <i class="bi bi-check-lg"></i></button></el-tooltip>
     </div>
   </div>
-  <el-dialog
-    v-model="dialogVisible"
-    :title="title"
-    width="30%"
-    :before-close="handleClose"
-  >
-    <p>发布日期 {{ releaseTime }}</p>
-    <span>{{ content }}</span>
-    <p :class="{deadline: urgent}">截止日期 {{ deadline }}</p>
-    <p>来自{{groupName}}</p>
+  <el-dialog v-model="dialogVisible" width="30%" :before-close="handleClose" center>
+    <template #title>
+      <h2>{{title}}</h2>
+      <div class="date">发布时间: {{ myReleaseTime.toLocaleString('af') }}</div>
+    </template>
+    <span class="content">{{ content }}</span>
+    <p><div class="deadline">截止日期: {{ myDeadline.toLocaleString('af') }}</div><div>来自{{groupName}}</div></p>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialogVisible = false">已知晓</el-button>
@@ -95,10 +66,10 @@ export default {
     releaseTime: String,
     deadline: String,
     starred: Boolean,
-    done:Boolean,
-    id:String,
-    userId:String,
-    groupId:String
+    done: Boolean,
+    id: String,
+    userId: String,
+    groupId: String
   },
   data() {
     return {
@@ -112,7 +83,7 @@ export default {
       myUserId: this.userId,
       dialogVisible: false,
       urgent: false,
-      groupName: ''                       
+      groupName: ''
     };
   },
   mounted() {
@@ -172,12 +143,12 @@ export default {
           console.log(err);
         });
     },
-    setgroupName(){
+    setgroupName() {
       console.log("groupid", this.groupId);
       axios
         .get(`http://localhost:8080/group?id=${this.groupId}`)
         .then((res) => {
-          console.log('group',res);
+          console.log('group', res);
           this.groupName = res.data.name;
         })
         .catch((err) => {
@@ -186,7 +157,7 @@ export default {
     },
     isUrgent() {
       if (this.isCompleted) {
-          return false
+        return false
       }
       const now = new Date()
       const deadline = new Date(this.myDeadline)
@@ -197,6 +168,11 @@ export default {
 </script>
   
 <style scoped>
+.urgent-img{
+  width: 150px;
+  transform: rotate(15deg);
+  opacity: 0.7;
+}
 .completion-overlay {
   position: absolute;
   top: 0;
@@ -206,42 +182,64 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: rgba(255, 255, 255, 0.8); /* 调整覆盖层的透明度和颜色 */
+  background-color: rgba(255, 255, 255, 0.8);
+  /* 调整覆盖层的透明度和颜色 */
 }
+
+.urgen-alarm {
+  position: absolute;
+  top: -10px;
+  right: 0px;
+}
+
 .task-card.completed {
   background-color: #eee;
   position: relative;
 }
+
 .task-card button {
   background-color: #eee;
 }
+
 .star :hover {
   cursor: pointer;
 }
+
 .group-name {
   font-size: 15px;
 }
-.unurgent{ 
+
+.unurgent {
   font-size: 15px;
   margin-top: 0%;
 }
+
 .deadline {
-  color: #ff6666; /* 淡红色 */
+  color: #ff6666;
+  /* 淡红色 */
 }
+
 .icon-and-title {
   display: flex;
   flex-direction: row;
 }
+
 .content {
-  font-size: 15px;
+  font-size: 16px;
+  color: #000;
 }
+
 .content-text {
   display: -webkit-box;
   -webkit-box-orient: vertical;
   overflow: hidden;
   -webkit-line-clamp: 2;
-  line-height: 2em; /* 行高，根据需要调整 */
+  line-height: 1.5em;
+  /* 行高，根据需要调整 */
+  height: 3em;
+  font-size: 18px;
 }
+
 .task-card {
   border-radius: 8px;
   padding: 16px;
@@ -250,15 +248,17 @@ export default {
   background-color: white;
   margin-left: 50px;
   box-shadow: var(--el-box-shadow-light);
+  position: relative;
 }
 
 .title {
   margin: 0;
-  font-size: 24px;
+  font-size: 22px;
   margin-left: 10px;
-  overflow: hidden;
   white-space: nowrap;
-  text-overflow: ellipsis;
+  /* 防止文本换行 */
+  overflow: hidden;
+  /* 隐藏溢出的文本 */
 }
 
 .date {
@@ -279,19 +279,21 @@ export default {
   flex: 1;
   border-radius: 4px;
 }
+
 .outside {
   margin: 0 100px 5px;
   padding: 0;
 }
+
 .footer button:not(:last-child) {
-  margin-right: 8px; 
+  margin-right: 8px;
 }
 </style>
 
 <style>
-.group-name a  {
+.group-name a {
 
-color: orange;
-font-weight: 400;
-} 
+  color: coral;
+  font-weight: 400;
+}
 </style>
